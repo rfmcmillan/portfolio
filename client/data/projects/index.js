b67fafa8b3c4b77c638f3d196ddfed490d67d935
Russel McMillan <rfmcmillan@gmail.com>
2022-01-23 20:42:28 -0500

    updated skills

--- client/data/projects/index.js
+++ client/data/projects/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { pomodoroGo, pocketTrade, globalSnacker } from '../../img/projects';
+import React from "react";
+import { pomodoroGo, pocketTrade, globalSnacker } from "../../img/projects";
 
-import { ExternalIcon, GithubIcon } from '../../img/icons';
+import { ExternalIcon, GithubIcon } from "../../img/icons";
 import {
   postgres,
   react,
@@ -22,15 +22,15 @@ import {
   alpaca,
   apexCharts,
   googleMaps,
-} from '../../img/logos';
+} from "../../img/logos";
 
 export const projectsData = [
   {
-    title: 'PomodoroGo',
+    title: "PomodoroGo",
     description: `A chrome extension that improves productivity by combining a web-site blocker, focus timer and data dashboard.`,
-    image: './assets/Dashboard.png',
-    role: 'Full Stack Developer',
-    type: 'Group Project',
+    image: "./assets/Dashboard.png",
+    role: "Full Stack Developer",
+    type: "Group Project",
     stack: [
       nodejs,
       react,
@@ -42,28 +42,62 @@ export const projectsData = [
       jwt,
       mocha,
       chrome,
+      html,
+      css,
     ],
     buttons: [
       {
-        text: 'View Site',
-        link: 'https://pomodoro-go-1.herokuapp.com/home',
-        linkAriaLabel: 'View the hosted version of PomodoroGo',
+        text: "View Site",
+        link: "https://pomodoro-go-1.herokuapp.com/home",
+        linkAriaLabel: "View the hosted version of PomodoroGo",
         icon: <ExternalIcon />,
       },
       {
-        text: 'View Repo',
-        link: 'https://github.com/rfmcmillan/pomodoro-go-1',
-        linkAriaLabel: 'View Github repository for PomodoroGo',
+        text: "View Repo",
+        link: "https://github.com/rfmcmillan/pomodoro-go-1",
+        linkAriaLabel: "View Github repository for PomodoroGo",
         icon: <GithubIcon />,
       },
     ],
   },
   {
-    title: 'Global Snacker',
+    title: "RapidRebalance",
+    description: `A portfolio management app that allows an investor to quickly rebalance their portfolio.`,
+    image: "./assets/RapidRebalance - Home.png",
+    role: "Full Stack Developer",
+    type: "Solo Project/Hackathon",
+    stack: [
+      nodejs,
+      react,
+      postgres,
+      redux,
+      express,
+      materialUi,
+      css,
+      html,
+      webpack,
+    ],
+    buttons: [
+      {
+        text: "View Site",
+        link: "https://rapid-rebalance.herokuapp.com/#/",
+        linkAriaLabel: "View the hosted version of RapidRebalance",
+        icon: <ExternalIcon />,
+      },
+      {
+        text: "View Repo",
+        link: "https://github.com/rfmcmillan/rapid-rebalance",
+        linkAriaLabel: "View Github repository for Rapid-Rebalance",
+        icon: <GithubIcon />,
+      },
+    ],
+  },
+  {
+    title: "Global Snacker",
     description: `An e-commerce site that sells snacks from around the world.`,
-    image: './assets/GlobalSnackerHome.png',
-    role: 'Full Stack Developer',
-    type: 'Group Project',
+    image: "./assets/GlobalSnackerHome.png",
+    role: "Full Stack Developer",
+    type: "Group Project",
     stack: [
       nodejs,
       react,
@@ -74,40 +108,20 @@ export const projectsData = [
       googleMaps,
       jwt,
       mocha,
+      html,
+      css,
     ],
     buttons: [
       {
-        text: 'View Site',
-        link: 'https://global-snacker-1.herokuapp.com/',
-        linkAriaLabel: 'View the hosted version of Global Snacker',
+        text: "View Site",
+        link: "https://global-snacker-1.herokuapp.com/",
+        linkAriaLabel: "View the hosted version of Global Snacker",
         icon: <ExternalIcon />,
       },
       {
-        text: 'View Repo',
-        link: 'https://github.com/rfmcmillan/Grace-Shopper-2101',
-        linkAriaLabel: 'View Github repository for Global Snacker',
-        icon: <GithubIcon />,
-      },
-    ],
-  },
-  {
-    title: 'RapidRebalance',
-    description: `A portfolio management app that allows an investor to quickly rebalance their portfolio.`,
-    image: './assets/RapidRebalance - Home.png',
-    role: 'Full Stack Developer',
-    type: 'Solo Project/Hackathon',
-    stack: [nodejs, react, postgres, redux, express, materialUi, alpaca],
-    buttons: [
-      {
-        text: 'View Site',
-        link: 'https://rapid-rebalance.herokuapp.com/#/',
-        linkAriaLabel: 'View the hosted version of RapidRebalance',
-        icon: <ExternalIcon />,
-      },
-      {
-        text: 'View Repo',
-        link: 'https://github.com/rfmcmillan/rapid-rebalance',
-        linkAriaLabel: 'View Github repository for Rapid-Rebalance',
+        text: "View Repo",
+        link: "https://github.com/rfmcmillan/Grace-Shopper-2101",
+        linkAriaLabel: "View Github repository for Global Snacker",
         icon: <GithubIcon />,
       },
     ],
